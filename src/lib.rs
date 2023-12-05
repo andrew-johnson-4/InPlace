17b15f7ea1e5cbf1f0914c6675abfe8858b5ad95
Andrew Johnson <andrew@subarctic.org>
2023-12-04 18:01:26 -0700

    add test cases for unification in relog

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+
+pub fn relog(s: &str) -> String {
+   s.to_string()
+}
---
+++ tests/a_relog.rs
@@ -0,0 +1,12 @@
+
+use inplace::relog;
+
+#[test]
+fn substitution() {
+   assert_eq!(relog("a=Int;T<a>"), "T<Int>");
+}
+
+#[test]
+fn unification() {
+   assert_eq!(relog("A<b,C<d>>=A<Int,C<Bool>>;R<b>"), "R<Int>");
+}
